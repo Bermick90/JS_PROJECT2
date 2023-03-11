1a51afeee7aafb0dce6c6b12c3df1afab0e3f155
Bermick90 <120055844+Bermick90@users.noreply.github.com>
2023-03-11 20:47:07 +0100

    first issue

---
+++ script.js
@@ -0,0 +1,34 @@
+const input_amount = document.getElementById('amount');
+const currency_select = document.getElementById('currencies');
+const result_value = document.getElementById('result');
+const check_button = document.getElementById('button');
+const currency_table = getCurrencies();
+const resultValue = document.getElementById('result');
+const select = document.getElementById('select');
+let currencies;
+let currencyPLN;
+
+resultValue.innerHTML = ' XXX PLN';
+
+function getCurrencies() {
+  fetch('http://api.nbp.pl/api/exchangerates/tables/A/')
+    .then((response) => response.json())
+    .then((data) => {
+      currencies = data[0].rates;
+    });
+}
+document.addEventListener('DOMContentLoaded', getCurrencies);
+
+function convertCurrency() {
+  currencies.forEach((item) => {
+    if (item.code === currency_select.value) {
+      const result = item.mid * input_amount.value;
+      console.log(item.mid);
+      result_value.innerHTML = result + ' PLN';
+    }
+  });
+}
+
+const button = document
+  .getElementById('button')
+  .addEventListener('click', convertCurrency);
