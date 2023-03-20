d7a2a08b3534d7211f676943361f89f2886fde24
Bermick90 <120055844+Bermick90@users.noreply.github.com>
2023-03-20 21:02:34 +0100

    update

--- script.js
+++ script.js
@@ -22,9 +22,12 @@ document.addEventListener('DOMContentLoaded', getCurrencies);
 function convertCurrency() {
   currencies.forEach((item) => {
     if (item.code === currencySelect.value) {
-      const result = item.mid * inputAmount.value;
-      const resultFixed = result.toFixed(2);
-      resultValue.innerHTML = resultFixed + ' PLN';
+      if (currencySelect.value <= 0) {
+      } else {
+        const result = item.mid * inputAmount.value;
+        const resultFixed = result.toFixed(2);
+        resultValue.innerHTML = resultFixed + ' PLN';
+      }
     }
   });
 }
